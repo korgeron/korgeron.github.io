434a2a58c4f449576e94d5ffc6fc5e257931ce61
kevin <orgeron.kevin@gmail.com>
2023-04-06 19:53:06 -0500

    setting up show projects button

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
-document.querySelector('.show-projects').addEventListener('click', ()=>{
+const showProjects = document.querySelector('.show-projects');
+
+showProjects.addEventListener('click', ()=>{
+    showProjects.style.display = "none";
+
     document.querySelectorAll('.projects').forEach(project => {
         project.style.animationName = 'loadIn';
-        project.style.animationDelay = '1s';
+        project.style.animationDelay = '.1s';
         project.style.animationDuration = '1.5s';
         project.style.animationFillMode = 'forwards';
     })
