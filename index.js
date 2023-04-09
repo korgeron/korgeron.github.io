a7cfe5de15fe1c0767e80a35cf85dda0db7af349
kevin <orgeron.kevin@gmail.com>
2023-04-09 15:30:31 -0500

    editing style

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-
-const showProjects = document.querySelector('.show-projects');
-
-showProjects.addEventListener('click', ()=>{
-    showProjects.style.display = "none";
-
-    document.querySelectorAll('.projects').forEach(project => {
-        project.style.animationName = 'loadIn';
-        project.style.animationDelay = '.1s';
-        project.style.animationDuration = '1.5s';
-        project.style.animationFillMode = 'forwards';
-    })
+"use strict";
+document.querySelectorAll('.projects').forEach(project => {
+    project.style.animationName = 'loadIn';
+    project.style.animationDelay = '1s';
+    project.style.animationDuration = '1.5s';
+    project.style.animationFillMode = 'forwards';
 })
+
