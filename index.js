c2a5f7adcaac7b09a3a4ec3181be5b875ba8b3c8
kevin <orgeron.kevin@gmail.com>
2023-04-09 16:09:58 -0500

    editing style

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ document.querySelectorAll('.projects').forEach(project => {
     project.style.animationFillMode = 'forwards';
 })
 
+document.querySelector('.greeting-title').addEventListener('mouseleave' , ()=>{
+document.querySelector('.span').style.color = "blue";
+document.querySelector('.greeting-h1').style.color = "white";
+})
+document.querySelector('.greeting-title').addEventListener('mouseenter', ()=>{
+document.querySelector('.span').style.color = "red";
+document.querySelector('.greeting-h1').style.color = "black";
+})
